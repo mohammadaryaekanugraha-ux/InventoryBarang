1f60928efacdd2330d76f7a780e4288af18e673d
M Arya Eka N <mohammadaryaekanugraha@gmail.com>
2026-05-20 20:44:08 +0700

    Upgrade Kode

--- kategori_produk.php
+++ kategori_produk.php
@@ -66,11 +66,11 @@ if (isset($_POST['update'])) {
     <header id="header" class="header fixed-top d-flex align-items-center">
 
         <div class="d-flex align-items-center justify-content-between">
-        <a href="index.php" class="logo d-flex align-items-center">
+        <a href="index.php" class="logo d-flex align-items-center me-auto me-lg-0" style="max-width: none; width: auto;">
             <img src="assets/img/logo.png" alt="">
-            <span class="d-none d-lg-block">Inventory_Barang_25550021</span>
+            <span class="d-none d-lg-block" style="font-size: 24px; padding-left: 8px;">Inventory_Barang_25550021</span>
         </a>
-        <i class="bi bi-list toggle-sidebar-btn"></i>
+        <i class="bi bi-list toggle-sidebar-btn" style="margin-left: 20px;"></i>
         </div><!-- End Logo -->
 
     
@@ -92,16 +92,6 @@ if (isset($_POST['update'])) {
                 </li>
 
                 <li>
-                <a class="dropdown-item d-flex align-items-center" href="logout.php">
-                    <i class="bi bi-box-arrow-right"></i>
-                    <span>Sign Out</span>
-                </a>
-                </li>
-                <li>
-                <hr class="dropdown-divider">
-                </li>
-
-                <li>
                 <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                     <i class="bi bi-gear"></i>
                     <span>Account Settings</span>
@@ -115,6 +105,16 @@ if (isset($_POST['update'])) {
                 <a class="dropdown-item d-flex align-items-center" href="pages-faq.html">
                     <i class="bi bi-question-circle"></i>
                     <span>Need Help?</span>
+                </a>
+                </li>
+                <li>
+                <hr class="dropdown-divider">
+                </li>
+
+                <li>
+                <a class="dropdown-item d-flex align-items-center" href="logout.php">
+                    <i class="bi bi-box-arrow-right"></i>
+                    <span>Sign Out</span>
                 </a>
                 </li>
                 <li>
